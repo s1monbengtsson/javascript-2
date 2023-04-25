3bd41a6084e7c4d7da8c660158ba3b8bd2530bdb
Simon <simonbengan8@gmail.com>
2023-04-25 14:39:20 +0200

    render a list of posts using index.

--- 01-react-basics/src/App.tsx
+++ 01-react-basics/src/App.tsx
@@ -1,13 +1,29 @@
 import './App.css'
 import { useState } from 'react'
 
+type Post = {
+	title: string
+	likes: number
+}
+
 const App = () => {
 	const [msg, setMsg] = useState("Hi mom, I'm stateful")
 	const [counter, setCounter] = useState(0)
+	const [posts, setPosts] = useState<Post[]>([
+		{ title: "React Rocks", likes: 1337},
+		{ title: "JSX Rocks even more", likes: 42},
+		{ title: "Got state!?", likes: 3},
+	])
+
+	const [isActive, setIsActive] = useState(false)
 
 	const handleButtonClick = () => {
-		setMsg("By mom")
+		setMsg("Bye mom")
 		setCounter(counter+1)
+	}
+
+	const handleClick = () => {
+		setIsActive(current => !current);
 	}
 
 	return (
@@ -19,6 +35,22 @@ const App = () => {
 			<p>You have clicked the button {counter} times</p>
 
 			<button onClick={handleButtonClick} className="btn btn-success btn-lg">👆 me!</button>
+
+			<button className={isActive ? 'red' : 'blue'} onClick={() => {
+				handleButtonClick()
+				handleClick()
+			}}>Try</button>
+
+			<ul>
+				{
+					posts.map((post, index) => (
+						<li key={index}>
+							<p>Title: {post.title} ({post.likes} likes 👍)</p>
+						</li>
+					))
+				}
+			</ul>
+
 		</div>
 	)
 }
