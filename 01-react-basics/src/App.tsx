a36ab163bbb95f9713e2c3255f8cab14d0ec5bfb
Simon <simonbengan8@gmail.com>
2023-04-25 13:42:38 +0200

    add counter for number of clicks.

--- 01-react-basics/src/App.tsx
+++ 01-react-basics/src/App.tsx
@@ -2,11 +2,12 @@ import './App.css'
 import { useState } from 'react'
 
 const App = () => {
-
 	const [msg, setMsg] = useState("Hi mom, I'm stateful")
+	const [counter, setCounter] = useState(0)
 
 	const handleButtonClick = () => {
 		setMsg("By mom")
+		setCounter(counter+1)
 	}
 
 	return (
@@ -15,7 +16,7 @@ const App = () => {
 
 			<h2>{msg}</h2>
 
-			<p>You have clicked the button 0 times</p>
+			<p>You have clicked the button {counter} times</p>
 
 			<button onClick={handleButtonClick} className="btn btn-success btn-lg">👆 me!</button>
 		</div>
