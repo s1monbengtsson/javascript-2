1d976bae057147ba76d0a5e5f47a34ce372700c1
Simon <simonbengan8@gmail.com>
2023-05-12 10:32:18 +0200

    fetch skeleton

---
+++ 04-fetch/src/App.tsx
@@ -0,0 +1,34 @@
+import { useState } from 'react'
+import './assets/scss/App.scss'
+
+function App() {
+	const [resource, setResource] = useState('posts')
+	const [data, setData] = useState([])
+
+	return (
+		<div className="container">
+			<h1 className="mb-3">Fetch</h1>
+
+			<div className="d-flex justify-content-between">
+				<button onClick={() => setResource('albums')} className="btn btn-primary">Albums</button>
+				<button onClick={() => setResource('photos')} className="btn btn-success">Photos</button>
+				<button onClick={() => setResource('posts')} className="btn btn-warning">Posts</button>
+				<button onClick={() => setResource('todos')} className="btn btn-danger">Todos</button>
+			</div>
+
+			{data && (
+				<>
+					<h2>{resource}</h2>
+					<p>There are {data.length} {resource}.</p>
+					<ol>
+						{data.map(item => (
+							<li>{item.title}</li>
+						))}
+					</ol>
+				</>
+			)}
+		</div>
+	)
+}
+
+export default App
