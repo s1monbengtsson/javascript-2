17899668ffa5b7a233e234a3b7e8f4188db03c79
Simon <simonbengan8@gmail.com>
2023-05-12 11:19:41 +0200

    fetch from api

--- 04-fetch/src/App.tsx
+++ 04-fetch/src/App.tsx
@@ -1,9 +1,33 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './assets/scss/App.scss'
+
+interface IResource {
+	id: number
+	title: string
+}
 
 function App() {
 	const [resource, setResource] = useState('posts')
-	const [data, setData] = useState([])
+	const [data, setData] = useState<IResource[]>([])
+
+	useEffect(() => {
+		if (!resource) {
+			return
+		}
+
+		// fetch resource
+		const fetchData = async () => {
+
+			// parse response as json
+			const res = await fetch(`https://jsonplaceholder.typicode.com/${resource}`)
+			const payload = await res.json() as IResource[]
+
+			// update data state with source payload
+			setData(payload)
+		}
+		fetchData()
+	}, [resource])
+
 
 	return (
 		<div className="container">
@@ -16,13 +40,13 @@ function App() {
 				<button onClick={() => setResource('todos')} className="btn btn-danger">Todos</button>
 			</div>
 
-			{data && (
+			{resource && (
 				<>
 					<h2>{resource}</h2>
 					<p>There are {data.length} {resource}.</p>
 					<ol>
 						{data.map(item => (
-							<li>{item.title}</li>
+							<li key={item.id}>{item.title}</li>
 						))}
 					</ol>
 				</>
