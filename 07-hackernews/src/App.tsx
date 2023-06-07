0ace203067a5e5e51161a4a295b0449009202760
Simon <simonbengan8@gmail.com>
2023-06-07 15:12:46 +0200

    added root route and HomePage.

--- 07-hackernews/src/App.tsx
+++ 07-hackernews/src/App.tsx
@@ -1,10 +1,22 @@
+import Container from 'react-bootstrap/Container'
+import { Route, Routes } from 'react-router-dom'
+import './assets/scss/App.scss'
+import HomePage from './pages/HomePage'
 
 const App = () => {
 
-  return (
-    <>
-      <h1>Hacker News 👀</h1>
-    </>
+return (
+	<div id="App">
+		{/* Navigation */}
+
+		<Container className="py-3">
+			<Routes>
+				<Route path="/" element={<HomePage />} />
+				{/* <Route path="/search" element={} /> */}
+			</Routes>
+		</Container>
+
+	</div>
   )
 }
 
---
+++ 07-hackernews/src/pages/HomePage.tsx
@@ -0,0 +1,16 @@
+import Button from 'react-bootstrap/Button'
+import { Link } from 'react-router-dom' 
+
+const HomePage = () => {
+  return (
+    <>
+        <h1>Welcome to Hacker News🕵️‍♀️👀</h1>
+
+        <Link to="/search">
+            <Button variant='primary'>Use the search, you must!</Button>
+        </Link>
+    </>
+  )
+}
+
+export default HomePage
