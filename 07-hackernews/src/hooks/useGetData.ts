e4057c50beb010da4f4e34f91b0f21988e57e60b
Simon <simonbengan8@gmail.com>
2023-06-08 15:14:38 +0200

    finished workshop fopr 06.08.

---
+++ 07-hackernews/src/hooks/useGetData.ts
@@ -0,0 +1,46 @@
+import {useEffect, useState} from 'react'
+import { DogAPI_RandomImageResponse } from '../types'
+import axios from 'axios'
+
+const useGetData = (initialUrl: string|null = null) => {
+	const [data, setData] = useState<DogAPI_RandomImageResponse|null>(null)
+	const [url, setUrl] = useState<string|null>(initialUrl)
+	const [isLoading, setIsLoading] = useState(false)
+	const [error, setError] = useState<string|null>(null)
+	const newKey = Math.random().toString(36).substring(7);
+
+	const getData = async (resourceUrl: string) => {
+		setIsLoading(true)
+		try {
+			const res = await axios.get<DogAPI_RandomImageResponse>(resourceUrl)
+
+			setData(res.data)
+			setIsLoading(false)
+			setError(null)
+			
+		} catch(err: any) {
+			setError(`Could not fetch data: ${err.message}`)
+			setIsLoading(false)
+		}
+
+	}
+
+	useEffect(() => {
+		if (!url) {
+			return
+		}
+		
+		getData(url)
+	}, [url])
+
+
+	return {
+		data,
+		setUrl,
+		newKey,
+		isLoading,
+		error
+	}
+}
+
+export default useGetData
--- 07-hackernews/src/pages/RandomDogPage.tsx
+++ 07-hackernews/src/pages/RandomDogPage.tsx
@@ -1,44 +1,34 @@
-import React, { useEffect, useState } from 'react'
-import axios from 'axios'
 import Image from 'react-bootstrap/Image'
 import Button from 'react-bootstrap/Button'
-import { DogAPI_RandomImageResponse } from '../types'
+
+import useGetData from '../hooks/useGetData'
+import Alert from 'react-bootstrap/Alert'
 
 
 const RandomDogPage = () => {
-	const [data, setData] = useState<DogAPI_RandomImageResponse|null>(null)
-	const [url, setUrl] = useState("")
-
-	const getData = async (resourceUrl: string) => {
-		const res = await axios.get<DogAPI_RandomImageResponse>(resourceUrl)
-			setData(res.data)
-	}
-
-	useEffect(() => {
-		if (!url) {
-			return
-		}
-		getData(url)
-	}, [url])
-
-
-	console.log("data:", data)
+	const { data, setUrl, newKey, isLoading, error } = useGetData("https://dog.ceo/api/breeds/image/random")
+	
 	return (
 		<>
 			<h1>A random doggo 🐶</h1>
 
 			<div>
-				<Button variant='primary' onClick={() => setUrl("https://dog.ceo/api/breeds/image/random")}>Random Doggo</Button>
-				<Button variant='primary' onClick={() => setUrl("https://dog.ceo/api/breed/chihuahua/images/random")}>Random Boxer Doggo</Button>
+				<Button variant='primary' className="mx-2" onClick={() => setUrl(`https://dog.ceo/api/breeds/image/random?key=${newKey}`)}>Random Doggo</Button>
+				<Button variant='primary' className="mx-2" onClick={() => setUrl(`https://dog.ceo/api/breed/chihuahua/images/random?key=${newKey}`)}>Random Chihuahua Doggo</Button>
 			</div>
 
-			{!data && <p>Loading...</p>}
+			{isLoading && <p>Loading...</p>}
+
+			{error && <Alert variant='warning'>{error}</Alert>}
+
+			{!error && (
+				<div>
+					{!isLoading && data && data.status === "success" && (
+						<Image src={data.message} className="img-fluid"></Image>
+					)}
+				</div>
+			)}
 			
-			<div>
-				{data && data.status === "success" && (
-					<Image src={data.message} className="img-fluid"></Image>
-				)}
-			</div>
 		</>
 	)
 }
