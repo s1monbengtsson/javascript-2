d889010c0c484bebb9e65217496cc253bc6ce695
sb <dev.simon.bengtsson@gmail.com>
2023-09-12 13:24:38 +0200

    centered content.

--- 13-firebase-todos/src/pages/LoginPage.tsx
+++ 13-firebase-todos/src/pages/LoginPage.tsx
@@ -10,6 +10,7 @@ import useAuth from '../hooks/useAuth'
 import { toast } from 'react-toastify'
 import { FirebaseError } from 'firebase/app'
 import { useState } from 'react'
+import { Container } from 'react-bootstrap'
 
 const LoginPage = () => {
     const [isLoading, setIsLoading] = useState(false)
@@ -37,61 +38,63 @@ const LoginPage = () => {
     }
 
     return (
-        <Row className='mt-3'>
-            <Col md={{ span: 6, offset: 3 }}>
-                <Card>
+        <Container className='center-y'>
+            <Row className='mt-3'>
+                <Col md={{ span: 6, offset: 3 }}>
+                    <Card>
 
-                    <Card.Body>
-                        <Card.Title className='mb-3'>Login</Card.Title>
+                        <Card.Body>
+                            <Card.Title className='mb-3'>Login</Card.Title>
 
-                        <Form onSubmit={handleSubmit(onLogin)}>
-                            <Form.Group controlId='email' className='mb-3'>
-                                <Form.Label>Email</Form.Label>
-                                <Form.Control
-                                    type='email'
-                                    placeholder='yomama@hotmail.com'
-                                    {...register('email', {
-                                        required: 'You have to enter an email'
-                                    })}
-                                />
-                                {errors.email && <p  className='invalid'>{errors.email.message ?? "Invalid email"}</p>}
-                            </Form.Group>
+                            <Form onSubmit={handleSubmit(onLogin)}>
+                                <Form.Group controlId='email' className='mb-3'>
+                                    <Form.Label>Email</Form.Label>
+                                    <Form.Control
+                                        type='email'
+                                        placeholder='yomama@hotmail.com'
+                                        {...register('email', {
+                                            required: 'You have to enter an email'
+                                        })}
+                                    />
+                                    {errors.email && <p  className='invalid'>{errors.email.message ?? "Invalid email"}</p>}
+                                </Form.Group>
 
-                            <Form.Group controlId='password' className='mb-3'>
-                                <Form.Label>Password</Form.Label>
-                                <Form.Control
-                                    autoComplete='new-password'
-                                    type='password'
-                                    {...register('password', {
-                                        required: "You're kidding, right? Enter a password...",
-                                    })}
-                                />
-                                {errors.password && <p  className='invalid'>{errors.password.message ?? "Invalid password"}</p>}
-                            </Form.Group>
+                                <Form.Group controlId='password' className='mb-3'>
+                                    <Form.Label>Password</Form.Label>
+                                    <Form.Control
+                                        autoComplete='new-password'
+                                        type='password'
+                                        {...register('password', {
+                                            required: "You're kidding, right? Enter a password...",
+                                        })}
+                                    />
+                                    {errors.password && <p  className='invalid'>{errors.password.message ?? "Invalid password"}</p>}
+                                </Form.Group>
 
 
-                            <Button 
-                                variant='primary' 
-                                type='submit' 
-                                disabled={isLoading}
-                                >{isLoading 
-                                    ? "Signing in..." 
-                                    : "Sign in"}
-                            </Button>
-                        </Form>
+                                <Button 
+                                    variant='primary' 
+                                    type='submit' 
+                                    disabled={isLoading}
+                                    >{isLoading 
+                                        ? "Signing in..." 
+                                        : "Sign in"}
+                                </Button>
+                            </Form>
 
-                        <div className="text-center">
-                            <Link to='/forgot-password'>Forgot Password</Link>
-                        </div>
-                    </Card.Body>
-                </Card>
+                            <div className="text-center">
+                                <Link to='/forgot-password'>Forgot Password</Link>
+                            </div>
+                        </Card.Body>
+                    </Card>
 
-                <div className="text-center mt-3">
-                    Don't have an account?
-                    <Link to='/signup' className='mx-1'>Sign Up</Link>
-                </div>
-            </Col>
-        </Row>
+                    <div className="text-center mt-3">
+                        Don't have an account?
+                        <Link to='/signup' className='mx-1'>Sign Up</Link>
+                    </div>
+                </Col>
+            </Row>
+        </Container>
     )
 }
 
--- 13-firebase-todos/src/pages/SignupPage.tsx
+++ 13-firebase-todos/src/pages/SignupPage.tsx
@@ -10,6 +10,7 @@ import { useRef, useState } from 'react'
 import useAuth from '../hooks/useAuth'
 import { toast } from 'react-toastify'
 import { FirebaseError } from 'firebase/app'
+import { Container } from 'react-bootstrap'
 
 const SignupPage = () => {
     const [errorMessage, setErrorMessage] = useState<string|null>(null)
@@ -48,86 +49,88 @@ const SignupPage = () => {
     }
 
     return (
-        <Row className='mt-3'>
-            <Col md={{ span:6, offset: 3 }}>
-                <Card>
+        <Container className='center-y'>
+            <Row>
+                <Col md={{ span:6, offset: 3 }}>
+                    <Card>
 
-                    <Card.Body>
-                        <Card.Title className='mb-3'>Sign Up</Card.Title>
+                        <Card.Body>
+                            <Card.Title className='mb-3'>Sign Up</Card.Title>
 
-                        {/* {errorMessage && <Alert variant='danger'>{errorMessage}</Alert>} */}
+                            {/* {errorMessage && <Alert variant='danger'>{errorMessage}</Alert>} */}
 
-                        <Form onSubmit={handleSubmit(onSignup)}>
-                            <Form.Group controlId='email' className='mb-3'>
-                                <Form.Label>Email</Form.Label>
-                                <Form.Control
-                                    type='email'
-                                    placeholder='yomama@hotmail.com'
-                                    {...register('email', {
-                                        required: 'You have to enter an email'
-                                    })}
-                                />
-                                {errors.email && <p  className='invalid'>{errors.email.message ?? "Invalid email"}</p>}
-                            </Form.Group>
+                            <Form onSubmit={handleSubmit(onSignup)}>
+                                <Form.Group controlId='email' className='mb-3'>
+                                    <Form.Label>Email</Form.Label>
+                                    <Form.Control
+                                        type='email'
+                                        placeholder='yomama@hotmail.com'
+                                        {...register('email', {
+                                            required: 'You have to enter an email'
+                                        })}
+                                    />
+                                    {errors.email && <p  className='invalid'>{errors.email.message ?? "Invalid email"}</p>}
+                                </Form.Group>
 
-                            <Form.Group controlId='password' className='mb-3'>
-                                <Form.Label>Password</Form.Label>
-                                <Form.Control
-                                    autoComplete='new-password'
-                                    type='password'
-                                    {...register('password', {
-                                        required: "You're kidding, right? Enter a password...",
-                                        minLength: {
-                                            value: 6,
-                                            message: "Please enter at least 3 characters"
-                                        }
-                                    })}
-                                />
-                                {errors.password && <p  className='invalid'>{errors.password.message ?? "Invalid password"}</p>}
-                                <Form.Text>Password must be at least 6 characters long</Form.Text>
-                            </Form.Group>
+                                <Form.Group controlId='password' className='mb-3'>
+                                    <Form.Label>Password</Form.Label>
+                                    <Form.Control
+                                        autoComplete='new-password'
+                                        type='password'
+                                        {...register('password', {
+                                            required: "You're kidding, right? Enter a password...",
+                                            minLength: {
+                                                value: 6,
+                                                message: "Please enter at least 3 characters"
+                                            }
+                                        })}
+                                    />
+                                    {errors.password && <p  className='invalid'>{errors.password.message ?? "Invalid password"}</p>}
+                                    <Form.Text>Password must be at least 6 characters long</Form.Text>
+                                </Form.Group>
 
-                            <Form.Group controlId='confirmPassword' className='mb-3'>
-                                <Form.Label>Confirm Password</Form.Label>
-                                <Form.Control
-                                    autoComplete='off'
-                                    type='password'
-                                    {...register('passwordConfirm', {
-                                        required: "Enter your password again...",
-                                        minLength: {
-                                            value: 6,
-                                            message: "Please enter at least 6 characters"
-                                        },
-                                        validate: (value) => {
-                                            return value === passwordRef.current || "The passwords does not match"
-                                        }
-                                    })}
-                                />
-                                {errors.passwordConfirm && <p  className='invalid'>{errors.passwordConfirm.message ?? "Invalid value"}</p>}
-                            </Form.Group>
+                                <Form.Group controlId='confirmPassword' className='mb-3'>
+                                    <Form.Label>Confirm Password</Form.Label>
+                                    <Form.Control
+                                        autoComplete='off'
+                                        type='password'
+                                        {...register('passwordConfirm', {
+                                            required: "Enter your password again...",
+                                            minLength: {
+                                                value: 6,
+                                                message: "Please enter at least 6 characters"
+                                            },
+                                            validate: (value) => {
+                                                return value === passwordRef.current || "The passwords does not match"
+                                            }
+                                        })}
+                                    />
+                                    {errors.passwordConfirm && <p  className='invalid'>{errors.passwordConfirm.message ?? "Invalid value"}</p>}
+                                </Form.Group>
 
-                            <Button 
-                                variant='primary' 
-                                type='submit' 
-                                disabled={isLoading}
-                                >{isLoading 
-                                    ? "Creating Account..." 
-                                    : "Create Account"}
-                            </Button>
-                        </Form>
+                                <Button 
+                                    variant='primary' 
+                                    type='submit' 
+                                    disabled={isLoading}
+                                    >{isLoading 
+                                        ? "Creating Account..." 
+                                        : "Create Account"}
+                                </Button>
+                            </Form>
 
-                        {/* <div className="text-center">
-                            <Link to='/forgot-password'>Forgot Password</Link>
-                        </div> */}
-                    </Card.Body>
-                </Card>
+                            {/* <div className="text-center">
+                                <Link to='/forgot-password'>Forgot Password</Link>
+                            </div> */}
+                        </Card.Body>
+                    </Card>
 
-                <div className="text-center mt-3">
-                    Already have an account?
-                    <Link to='/login' className='mx-1'>Login?</Link>
-                </div>
-            </Col>
-        </Row>
+                    <div className="text-center mt-3">
+                        Already have an account?
+                        <Link to='/login' className='mx-1'>Login?</Link>
+                    </div>
+                </Col>
+            </Row>
+        </Container>
     )
 }
 
